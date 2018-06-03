55a89e7a8846eab1b7f4b1beefa185927483cabb
Mohit Sharma <Mohit_Sharma@mckinsey.com>
2018-06-03 11:39:08 +0530

    done

---
+++ src/main/java/Automation/RegressionSuite/testClass.java
@@ -0,0 +1,11 @@
+package Automation.RegressionSuite;
+
+import org.testng.annotations.Test;
+
+public class testClass {
+	@Test
+	public void loginTest() {
+		System.out.println("login test");
+	}
+
+}
